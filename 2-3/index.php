777ef562292dd0074105ec2a1120b06c68acad93
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-11 16:43:14 +0900

    定数 課題

---
+++ 2-3/index.php
@@ -0,0 +1,23 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+<?php
+echo "1. デプロイ"."<br>";
+echo "  デプロイ:　主にネットワークを通じて提供されるWebアプリケーションなどのシステム開発工程において、システムを利用可能な状態にすること  "."<br>";
+echo "2. バリデーション"."<br>";
+echo " バリデーション：対象がその仕様や文法などに照らして適切に記述・構築されているか否かを検証するという意味"."<br>";
+echo "1. アジャイル・ウォーターフォール"."<br>";
+echo " アジャイル:システムやソフトウェア開発におけるプロジェクト開発手法のひとつで、大きな単位でシステムを区切ることなく、小単位で実装とテストを繰り返して開発を進める　"."<br>";
+echo " ウォーターフォール:最初に全体の機能設計・計画を決定し、この計画に従って開発・実装していく手法　"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
