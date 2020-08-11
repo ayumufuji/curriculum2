b90f39f0481c8b9d3cbc51c04ec8a3a33cea92b6
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-11 18:37:57 +0900

    ループ文

---
+++ 2-6/index.php
@@ -0,0 +1,17 @@
+<?php
+
+for($i=0;$i<100;$i++) { 
+    echo $i;
+}
+echo '<br>';
+?>
+<?php
+
+echo "1.　サブクエリ　"."<br>";
+echo "サブクエリ　:　データベースなどの問い合わせ（クエリ）文の内部に含まれる、別の問い合わせ文　　　　"."<br>";
+echo "2.　Bootstrap　"."<br>";
+echo "Bootstrap　：　BootstrapはHTML／CSS／JavaScriptから構成される最も有名なWEBフレームワーク"."<br>";
+echo "３.　ライブラリ・フレームワーク　"."<br>";
+echo "ライブラリ：　　便利なプログラムの部品をいっぱい集めて、ひとまとめにしたファイルのこと"."<br>";
+echo "フレームワーク　：　全体の処理の流れが実装されており、その中の一部の具体的な処理を自分で実装して、はめ込めるようになっているシステム"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
