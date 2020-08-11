757545bcca55890ead67007bf87da2420aeb7442
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-11 18:08:32 +0900

    条件分岐

---
+++ 2-5/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$name = 'taro';
+$password = 'pass';
+
+if ($name == 'taro' && $password == 'pass') {
+    echo "ログイン成功です。";
+} elseif ($name == 'taro')  { 
+    echo "パスワードが間違っています。";
+} elseif ($password == 'pass') {
+    echo "名前が間違っています。";
+} else {
+    echo " 入力情報が間違っています ";
+}
+
+
+?>
+<?php
+echo "1.　IDE　"."<br>";
+echo "IDE　:　ソフトウェア開発に必要なソフトウェアを一つに組み合わせ、同じ操作画面から統一的な操作法で利用できるようにしたソフトウェアパッケージ　　　　"."<br>";
+echo "2.　JOIN　"."<br>";
+echo "JOIN　：　複数テーブルの結合を行いたいときに使用する命令"."<br>";
+echo "３.　コンフリクト　"."<br>";
+echo "コンフリクト　：　複数の装置やプログラムなどが同じ資源を同時に利用しようとして競合状態になってしまうことの意味"."<br>";
+
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
