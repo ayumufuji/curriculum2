a2dc2a5b3b096447553ac514e15252cc1672f594
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-11 23:29:06 +0900

    foreach

---
+++ 2-8/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$fruits = ["apple" =>"りんご", "orange" => "みかん", "peach" => "もも"];
+
+foreach ($fruits as $key => $value) {
+    echo $key ;
+    echo "といったら";
+    echo $value ;
+   
+}
+
+?>
+<?php
+echo '<br>';
+echo "1.　トランザクション　"."<br>";
+echo "トランザクション　:ソフトウェアの処理方式の一つで、互いに関連・依存する複数の処理をまとめ、一体不可分の処理単位として扱うこと"."<br>";
+echo "2.　排他ロック　"."<br>";
+echo "　排他ロック　：　データベースシステムなどで記憶領域への同時アクセスを制限するロック機構の一つで、他の実行主体によるアクセスを完全に禁止するもの　"."<br>";
+echo "３.　 チューニング　"."<br>";
+echo "チューニング：　コンピュータ、ソフトウェアなどの設定や構成を調整し、目標の状態に近づけたり、性能を最大限引き出したりする作業"."<br>";
+
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
