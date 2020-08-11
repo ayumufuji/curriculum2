bac92bdf51a4411520cbf6abdfba45e371e5afc9
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-11 22:56:40 +0900

    配列、連想配列

---
+++ 2-7/index.php
@@ -0,0 +1,39 @@
+<?php
+$color = ["red" => "赤", "blue" => "青", "green" => "緑"];
+
+echo $color["red"];
+
+echo $color["blue"];
+
+echo $color["green"];
+
+var_dump($color);
+?>
+<?php
+echo '<br>';
+
+$color = ["red" => "赤", "blue" => "青", "green" => "緑","yellow"=>"黄色"];
+
+$fruits["yellow"] = "黄色";
+
+echo $color["red"];
+
+echo $color["blue"];
+
+echo $color["green"];
+
+echo $color["yellow"];
+
+var_dump($color);
+?>
+
+<?php
+echo '<br>';
+echo "1.　プルリクエスト（マージリクエスト）　"."<br>";
+echo "プルリクエスト　:　開発者のローカルリポジトリで加えた変更を、他の開発者に知らせ、レビューしてもらう機能　　　　"."<br>";
+echo "2.　Git Flow　"."<br>";
+echo "Git Flow　：　チームで Git を使う際に役立つ、運用ルールをまとめたツール　"."<br>";
+echo "３.　 CRON　"."<br>";
+echo "CRON：　多くのUNIX系OSで標準的に利用される常駐プログラム（デーモン）の一種で、利用者の設定したスケジュールに従って指定されたプログラムを定期的に起動してくれるもの。"."<br>";
+
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
