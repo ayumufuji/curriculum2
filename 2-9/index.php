78a946a5255e0244ef24cd668fac7e65948b758e
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-12 00:17:28 +0900

    FizzBuzz問題

---
+++ 2-9/index.php
@@ -0,0 +1,29 @@
+<?php
+
+for($i=0;$i<100;$i++) { 
+
+if ($i == 0 ) {
+        echo 0;
+    } elseif ($i%3 == 0  && $i%5 == 0) {
+        echo "FizzBuzz!!";
+    } elseif ($i%3 == 0)  { 
+        echo "Fizz!";
+    } elseif ($i%5 == 0) {
+        echo "Buzz!";
+    } else {
+        echo $i;
+    }
+    }
+
+echo '<br>';
+
+?>
+<?php
+echo '<br>';
+echo "1.　パフォーマンス　"."<br>";
+echo "パフォーマンス　:　コンピュータなどの機器やソフトウェア、システムなどの処理性能や実行速度、通信回線・ネットワークなどの伝送速度・容量などのこと";"."<br>";
+echo "2.　スロークエリ　"."<br>";
+echo "　スロークエリ　：　データベースにおいて、時間のかかっているSQL　"."<br>";
+echo "３.　 クエリログ　"."<br>";
+echo "クエリログ：　mysqld の実行内容の一般的な記録"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
