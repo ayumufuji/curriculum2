6c85d1d8df25f858e2c8b9fe50e4f2204423f617
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-12 15:51:28 +0900

    便利な関数（日付・時間）

---
+++ 2-15/index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo strtotime("2017/12/31 23:59:59");
+echo '<br>';
+
+echo date("Y-m-d H:i:s", 1514761199);
+echo '<br>';
+
+
+
+
+
+
+echo "1.　ログ(意味、確認の仕方)　"."<br>";
+echo " ログ　:　コンピュータの記録のことで、イベントビューアーを起動することで確認できる　　　"."<br>";
+echo "2.  クラウドサーバー、レンタルサーバー、VPSの違い　"."<br>";
+echo "　クラウドサーバー　：　複数の仮想サーバーを個別で利用"."<br>";
+echo "　レンタルサーバー　：　１つの物理サーバーを複数ユーザーで共用"."<br>";
+echo "　VPS　：　１つの仮想サーバー群を個別で利用"."<br>";
+echo "３.　 PHPのFWの種類と特徴　"."<br>";
+echo "　種類　：　　パケットフィルタリング型ファイアウォール・アプリケーションゲートウェイ型ファイアウォール"."<br>";
+echo "　パケットフィルタリング型ファイアウォール　：　インターネットのデータ内容の単位である「パケット」を対象にしながら、事前に決められていないデータファイル等を未然に取り除く事が可能　"."<br>";
+echo "　アプリケーションゲートウェイ型ファイアウォール：　「プロキシサーバ」を、ファイアウォールとして使用することで、社内ネットワーク(イントラネット)とインターネットを安全に接続する事が可能　"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
