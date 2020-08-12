c0313c07abe6e99b08b8f3afba7b6fbc3b1c3998
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-12 10:35:07 +0900

    関数

---
+++ 2-10/index.php
@@ -0,0 +1,21 @@
+<?PHP
+
+function getTriangleArea($vertical,$width ,$height) {
+    $volume = $vertical * $width * $height ;
+    print "直方体の体積は". $volume ."だよ。";
+}
+
+getTriangleArea(5,10,8);
+
+?>
+
+
+<?php
+echo '<br>';
+echo "1.　ハッシュ化　"."<br>";
+echo "ハッシュ化　:　元のデータから一定の計算手順に従って ハッシュ値 と呼ばれる規則性のない固定長の値を求め、その値によって元のデータを置き換えること。"."<br>";
+echo "2.　総合テスト　"."<br>";
+echo "　総合テスト　：　、開発中のソフトウェアや情報システムのテスト手法の一つで、開発の最終段階にシステム全体を対象に行われるテスト。"."<br>";
+echo "３.　 デバック　"."<br>";
+echo "　デバック　：　コンピュータプログラムに潜む欠陥を探し出して取り除くこと。"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
