ee8d3c929172ea49d4c92b83acc41a83db0b5b2b
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-12 16:26:25 +0900

    フォームに入力した内容を取得する

--- 2-11/index.php
+++ 2-11/index.php
@@ -3,5 +3,19 @@
   <br>
   パスワード：<input type="password" name="password" />
   <br>
+  メールアドレス：<input type="text" name="email" />
+  <br>
   <input type="submit" value="送信" />
 </form>
+<?php
+echo '<br>';
+echo "1.　仕様書・設計書　"."<br>";
+echo " 　仕様書　:　ソフトウェア、情報システム、サービスなどを開発する際に、備えるべき機能やその性能、特性、満たすべき要件などを図表や文章などで記述した文書　　　"."<br>";
+echo " 　設計書　:　どうやったら実現できるかその実現方法を記載した書類　"."<br>";
+echo "2.  Git　"."<br>";
+echo "　Git　：　オープンソースの分散バージョン管理システムの一つ"."<br>";
+echo "３.　 マージツール　"."<br>";
+echo "　マージツール　：　バージョン管理システムなどで管理している開発中のプログラムなどに、新たにコードを追加したり、修正を反映させたりするツール"."<br>";
+
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+
