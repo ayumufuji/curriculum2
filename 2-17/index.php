24a6717401b29008d175aa1b6d41c3c31e00af44
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-14 21:37:59 +0900

    EXTRA問題

--- 2-17/index.php
+++ 2-17/index.php
@@ -3,27 +3,43 @@
 $n = 0;
 $s = 1;
 $sum = 0;
-$a = $sum += $n;
-$b = 40 -= $sum;
+
+
 while($sum < 40) {
     $n = mt_rand(1, 6);
     echo '<br>';
     $sum += $n;
-    
-    
-    if($a > 40) {
-        echo $s.'回目='.$b;
-        echo '合計'.$s.'回でゴールしました。'
-        break;
-    }
-    if($sum = 40) {
+    if($sum >= 40) {
         echo $s.'回目='.$n;
-        echo '合計'.$s.'回でゴールしました。'
+        echo '<br>';
+        echo '合計'.$s.'回でゴールしました。';
         break;
     }
     echo $s.'回目='.$n;
-    $s++;    
+    echo '<br>';
+    $s++; 
     }  
-
     
+    $times = date("H", time());
+
+    echo '<br>';
+    echo '<br>';
+    if(10 > $times && $times > 0 ) {
+    echo "今".$times."時台です。";
+    echo '<br>';
+    echo "おはようございます";
+    }
+
+    if(18 > $times && $times > 10) {
+    echo "今".$times."時台です。";
+    echo '<br>';
+    echo "こんにちは";
+    }
+
+    if(18 > $times && $times > 23 ) {
+    echo "今".$times."時台です。";
+    echo '<br>';
+    echo "こんばんは";
+    }
+
 ?>
