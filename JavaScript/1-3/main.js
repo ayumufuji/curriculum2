bfeb1d0945d02dccd90daaa8e38492d5e82d1e30
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-15 19:23:07 +0900

    条件分岐・演算子

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,17 @@
+let a = 11;
+let b = 16;
+
+console.log(a != 10 );
+console.log(20>b && b>=10 && b % 2 === 0);
+let x = 10;
+if (x <=20 && x >= 10) {
+    console.log('成功です');
+} else {
+    console.log('失敗です');  
+}
+let y = 0;
+if (y % 2 === 0) {
+    console.log('偶数です');
+} else {
+    console.log('奇数です');  
+}
