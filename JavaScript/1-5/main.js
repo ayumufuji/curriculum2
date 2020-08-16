aa31ca0a7bf512395f677f1d6042cadd5703c5a2
ayumufuji <68733701+ayumufuji@users.noreply.github.com>
2020-08-16 12:22:10 +0900

    関数

---
+++ JavaScript/1-5/main.js
@@ -0,0 +1,12 @@
+
+
+function createJuice(fruits) {
+    console.log(fruits + 'を受け取りました。ジュースにして返します。');
+    cook = fruits + 'ジュース'
+    return cook ;
+}
+
+
+let orangeJuice = createJuice('みかん');
+console.log(cook + 'が届きました。 ');
+
